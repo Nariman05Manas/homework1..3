70c1d65b17edd8000892404fefde938737dc8503
Nariman05Manas <89251464474@mail.ru>
2022-03-03 12:53:21 +0700

    задание 2.2

--- Homework1-3/Profile/ProfileViewController.swift
+++ Homework1-3/Profile/ProfileViewController.swift
@@ -7,13 +7,33 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         title = "Мой профиль"
         let profileHeaderView = ProfileHeaderView()
-        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(profileHeaderView)
-        
-    }
-    override func viewWillLayoutSubviews() {
-        self.view.subviews.first?.frame = self.view.frame
-    
+        view.addSubview(profileHeaderView)
+
+         profileHeaderView.setupView()
+
+         let titleButton = UIButton()
+         titleButton.translatesAutoresizingMaskIntoConstraints = false
+         titleButton.backgroundColor = .blue
+         titleButton.setTitle("Установить новый профиль", for: .normal)
+         titleButton.setTitleColor(.white, for: .highlighted)
+         titleButton.addTarget(self, action: #selector(setNewTitle), for: .touchUpInside)
+
+         view.addSubview(titleButton)
+
+         titleButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+         titleButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+         titleButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+         titleButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+     }
+    @objc func setNewTitle() {
+         if title == "Профиль" {
+             title = "попробуй пройти"
+         } else {
+             title = "Профиль"
+         }
+     }
+
  }
     
-}
+
