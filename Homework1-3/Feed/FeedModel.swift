67a102c903992cd3388b7fadf4f72b94c6ab54a6
Nariman05Manas <89251464474@mail.ru>
2022-06-10 10:59:24 +0300

    д/з 11 готово

--- Homework1-3/Feed/FeedModel.swift
+++ Homework1-3/Feed/FeedModel.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class FeedModel {
     
-    var infoArray = ["знакомьтесь это моя банда","моя прелесть"]
+    var infoArray = ["знакомьтесь это моя банда","моя прелесть!"]
     
     weak var coordinator: FeedCoordinator?
     
